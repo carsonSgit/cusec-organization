4a66d740d7ef10174951ae1c585e37e6976bd6e8
Angel Shinh <angelshinh2023@gmail.com>
2026-05-19 16:50:29 -0400

    feat: tab nav for schools

--- app/participant-schools/page.tsx
+++ app/participant-schools/page.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "@/components/Footer";
-import { LogoTile } from "@/components/LogoTile";
 import { PageShell } from "@/components/PageShell";
 import { SectionHeading } from "@/components/SectionHeading";
+import { ParticipantSchoolsBrowser } from "@/components/ParticipantSchoolsBrowser";
 import { regionsData } from "@/lib/schoolsData";
 
 export default function ParticipantSchoolsPage() {
@@ -20,41 +20,7 @@ export default function ParticipantSchoolsPage() {
               align="left"
             />
 
-            <div className="cusec-schools-regions">
-              {regionsData.map((region) => (
-                <div
-                  key={region.name}
-                  className="cusec-schools-region"
-                  style={{ marginTop: "4rem" }}
-                >
-                  <h3 className="cusec-schools-region-title" style={{ textAlign: "left" }}>
-                    {region.name}
-                  </h3>
-                  <div className="cusec-archive-list">
-                    {region.schools.map((school, idx) => (
-                      <article key={`${region.name}-${idx}`} className="cusec-archive-item">
-                        <div className="cusec-archive-item__header">
-                          <div className="cusec-archive-item__brand cusec-historic-sponsor__brand">
-                            {school.logo ? (
-                              <LogoTile
-                                name={school.name}
-                                logo={school.logo}
-                                variant="compact-left"
-                              />
-                            ) : null}
-                            <h3>{school.name}</h3>
-                          </div>
-                        </div>
-
-                        <div className="cusec-archive-item__content">
-                          <p>{school.info}</p>
-                        </div>
-                      </article>
-                    ))}
-                  </div>
-                </div>
-              ))}
-            </div>
+            <ParticipantSchoolsBrowser regions={regionsData} />
           </div>
         </section>
       </PageShell>
